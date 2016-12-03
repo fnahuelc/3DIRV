99bd6d39ddf0eec3d0f99727885a6dfd888bec27
fnahuelc <fnahuelc@gmail.com>
2016-12-03 02:31:30 -0300

    problema con mobile

--- js/script.js
+++ js/script.js
@@ -44,20 +44,7 @@
 /* Smooth Scrolling
 ------------------------------------------------------ */
 
-   $('.smoothscroll').on('click',function (e) {
-	    e.preventDefault();
-
-	    var target = this.hash,
-	    $target = $(target);
-
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 800, 'swing', function () {
-	        window.location.hash = target;
-	    });
-
-	});
-
+  
 
 /*----------------------------------------------------*/
 /* Appear Animation
